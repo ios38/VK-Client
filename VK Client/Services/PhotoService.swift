23e0219be6c6c4438a88365d7fc3d8b7da821110
Maksim Romanov <ios38ru@gmail.com>
2020-01-31 10:04:16 +0800

    Set up PhotoService

---
+++ VK Client/Services/PhotoService.swift
@@ -0,0 +1,81 @@
+//
+//  PhotoService.swift
+//  VK Client
+//
+//  Created by Maksim Romanov on 30.01.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import PromiseKit
+
+class PhotoService {
+    private var cacheLifeTime: TimeInterval = 60*60*24*7
+    private static var memoryCache = [String: UIImage]() //запретить многопоточную запись
+    private var isolatedQueue = DispatchQueue(label: "ru.maksim-romanov.vk-client.memoryCache")
+    
+    private var imageCacheUrl: URL? = {
+        let dirName = "Images"
+        
+        guard let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
+        let url =  cacheDir.appendingPathComponent(dirName, isDirectory: true)
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                return nil
+            }
+        }
+        return url
+    }()
+    
+    private func getFilePath(urlString: String) -> URL? {
+        let fileName = urlString.split(separator: "/").last ?? "default.png"
+        guard let imageCacheUrl = self.imageCacheUrl else { return nil }
+        return imageCacheUrl.appendingPathComponent(String(fileName))
+    }
+    
+    private func saveImageToFilesystem(urlString: String, image: UIImage) {
+        guard let data = image.pngData(),
+            let fileUrl = getFilePath(urlString: urlString) else { return }
+        
+        FileManager.default.createFile(atPath: fileUrl.absoluteString, contents: data, attributes: nil)
+    }
+    
+    private func loadImagesFromFilesystem(urlString: String) -> UIImage? {
+        guard let fileUrl = getFilePath(urlString: urlString),
+            let info = try? FileManager.default.attributesOfItem(atPath: fileUrl.absoluteString),
+            let modificationDate = info[.modificationDate] as? Date else { return nil }
+        
+        let imageLifeTime = Date().distance(to: modificationDate)
+        
+        guard imageLifeTime < cacheLifeTime,
+            let image = UIImage(contentsOfFile: fileUrl.absoluteString) else { return nil }
+        isolatedQueue.async {
+            PhotoService.memoryCache[urlString] = image
+        }
+        return image
+    }
+    
+    private func loadImage(urlString: String) -> Promise<UIImage> {
+        Alamofire.request(urlString)
+            .responseData()
+            .map { (data, _) -> UIImage in
+                guard let image = UIImage(data: data) else { throw PMKError.badInput }
+                return image }
+            .get(on: isolatedQueue) { PhotoService.memoryCache[urlString] = $0 }
+            .get { self.saveImageToFilesystem(urlString: urlString, image: $0) }
+    }
+    
+    public func photo(urlString: String) -> Promise<UIImage> {
+        if let image = PhotoService.memoryCache[urlString] {
+            return Promise.value(image)
+        } else
+            if let image = loadImagesFromFilesystem(urlString: urlString) {
+            return Promise.value(image)
+        }
+        return loadImage(urlString: urlString)
+    }
+}
